0834aed7665c8be5051f57d79b7ced7c29a56473
Simon Chanzu <chanzusimon6@gmail.com>
2023-05-19 02:29:49 +0300

    upload of react routing assignment

--- src/App.jsx
+++ src/App.jsx
@@ -1,76 +1,38 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
+// import reactLogo from './assets/react.svg'
+import { BrowserRouter,Route,Routes } from "react-router-dom";
+import Header from "./components/header";
+import Footer from "./components/Footer";
+import Blog1 from "./pages/Blog1" ;
+import Blog2 from "./pages/Blog2";
+import Blog3 from "./pages/Blog3";
+import Blog4 from "./pages/Blog4";
+import Blog5 from "./pages/Blog5";
+import NotFound from "./pages/NotFound";
+import "./app.css"
 
-function Title() {
-  return (
-    <div className="head">
-      <h2>A blog from a sample campus students</h2>
-    </div>
-  );
-}
-
-function Blogs({ name, blog, county }) {
-  return (
-    <div className="blog">
-      <h2>{name}</h2>
-      <p>{blog}</p>
-      <h4>{county}</h4>
-    </div>
-  );
-}
-
-function Highlights({ first, description }) {
-  return (
-    <div className="highlights">
-      <h5>{first}</h5>
-
-      <p>{description}</p>
-    </div>
-  );
-}
-
-function Footer() {
-  return (
-    <div className="footer">
-      <p>Simon maina</p> <input type="date"></input>
-    </div>
-  );
-}
 
 function App() {
-  const [count, setCount] = useState(0);
+  
 
   return (
-    <div>
-      <Title />
-      <Blogs
-        name="Maria"
-        blog="The MEd MESA program was an excellent addition to my primary PhD program in Literacy, Language, and Culture and has opened up so many new career possibilities to me in educational research."
-        county="Nairobi"
-      />
-      <Blogs
-        name="Jamiel"
-        blog="his program has been great so far. The faculty is supportive and readily available. The courses and individual lessons have been clear and coherent"
-        county="Kisumu"
-      />
-      <Blogs
-        name="Athman"
-        blog="The MEd MESA program is exactly the type of program I was looking for: It could be completed at distance, the course offerings are comprehensive in nature, but the material is presented in a very practical way"
-        county="Taita"
-      />
+    <div className="container" >
 
-      <Blogs />
+      <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path="/"       element={<Blog1/>}/>
+        <Route path="/blog2"  element={<Blog2/>} />
+        <Route path="/blog3"  element={<Blog3/>} />
+        <Route path="/blog4" element={<Blog4/>}/>
+        <Route path="/blog5" element={<Blog5/>}/>
+        <Route path="*"       element={< NotFound />} />
 
-      <Highlights
-        first="HIGHLIGHTS"
-        description="Physiology is the study of how the human body works. It describes the chemistry and physics behind basic body functions, from how molecules behave in cells to how systems of organs work together"
-      />
-      {/* <Highlights first="Jamiel" description="Orthopedic student"/>
-      <Highlights first="Athman" description="Physiotherapy student"/> */}
-      <Highlights />
-      <Footer />
+      </Routes>
+      <Footer/>
+      </BrowserRouter>
     </div>
 
   );
